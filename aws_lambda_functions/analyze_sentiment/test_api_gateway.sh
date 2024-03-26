efdd7881ca392074cf2e46d4ff1b4b7302a58bf8
Dominic Frei <dominic.frei@mongodb.com>
2024-03-26 23:38:03 +0900

    Updated app ids.

--- aws_lambda_functions/analyze_sentiment/test_api_gateway.sh
+++ aws_lambda_functions/analyze_sentiment/test_api_gateway.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 query="This was an amazing evening. Food, service, interior great."
-encoded_query=$(echo -n "$query" | jq -sRr @uri)
 
-invoke_url="https://qz3pgn7nd5.execute-api.us-east-1.amazonaws.com/PROD/sentiment?query=$encoded_query"
-echo "$invoke_url"
+invoke_url=https://qz3pgn7nd5.execute-api.us-east-1.amazonaws.com/PROD/sentiment
 
-curl -X GET "$invoke_url"
+curl -X POST -H "Content-Type: application/json" -d "{\"query\":\"$query\"}" "$invoke_url"
+
+echo ""
